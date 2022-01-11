c259cbcc6fea83ffd222552337f8e0cdccba1d7f
razvan <razvan.savu@typingdna.com>
2022-01-11 12:44:14 +0200

    Replace user model with django's

--- library/models.py
+++ library/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Book(models.Model):
@@ -8,12 +9,6 @@ class Book(models.Model):
         return len(Rental.objects.filter(book=self)) == 0
     def __str__(self):
         return self.title + ' - ' + self.author
-
-class User(models.Model):
-    id = models.CharField(max_length=13, primary_key=True)
-    name = models.CharField(max_length=64)
-    def __str__(self):
-        return self.name
 
 class Rental(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, unique=True)
