46198fe7f82f3e9f99eedf44240c0813860a11d4
razvan <razvan.savu@typingdna.com>
2022-01-11 12:03:16 +0200

    List books and enable search

--- library/views.py
+++ library/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Book
+from django.db.models import Q
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.POST.get('search'):
+        search_str = request.POST['search']
+        books = Book.objects.filter(Q(title__contains=search_str) | Q(author__contains=search_str))
+    else:
+        books = Book.objects.all()
+    context = {
+        'books': books,
+    }
+    return render(request, 'library/index.html', context)
