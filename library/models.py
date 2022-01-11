2440906414b3487177a6e2632831181a60886df4
razvan <razvan.savu@typingdna.com>
2022-01-11 12:25:47 +0200

    Add rental model. Show book availability.

--- library/models.py
+++ library/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=64)
     author = models.CharField(max_length=64)
+    def is_available(self):
+        return len(Rental.objects.filter(book=self)) == 0
     def __str__(self):
         return self.title + ' - ' + self.author
 
@@ -12,3 +14,9 @@ class User(models.Model):
     name = models.CharField(max_length=64)
     def __str__(self):
         return self.name
+
+class Rental(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    return_date = models.DateTimeField()
+
