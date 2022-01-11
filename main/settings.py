bac548d3cc93839dff96d178d26d36a76fb59bbf
razvan <razvan.savu@typingdna.com>
2022-01-11 12:02:32 +0200

    link app in main project

--- main/settings.py
+++ main/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_rename_app',
+    'library.apps.LibraryConfig',
 ]
 
 MIDDLEWARE = [
